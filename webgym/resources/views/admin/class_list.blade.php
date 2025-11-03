e5c28b5e28ee1680058603d6970727e01ca78ba0
Thanh Mai <151462737+Mywayyy@users.noreply.github.com>
2025-11-03 15:10:49 +0700

    phần dashboard
    nhớ chạy lệnh npm install

---
+++ webgym/resources/views/admin/class_list.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.ad_layout')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<h1>Đây là nội dung chính class_list</h1>
+
+@endsection
---
+++ webgym/resources/views/admin/store.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.ad_layout')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+<h1>Đây là nội dung chính store </h1>
+
+@endsection
