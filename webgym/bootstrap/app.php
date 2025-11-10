85679490240b79e631f5153419e9332335a053ce
Thanh Mai <151462737+Mywayyy@users.noreply.github.com>
2025-11-10 22:55:09 +0700

    Squashed commit of the following:
    commit 50ec65e7b9dc8f214d2057feb2c9875fdc95f69a
    Merge: 341ce6e 5fd1ac7
    Author: BuiPhuong2904 <phuongbui290405@gmail.com>
    Date:   Mon Nov 10 22:19:31 2025 +0700
    
        Merge remote-tracking branch 'origin/feature/admin_dashboard' into develop
    
    commit 341ce6eae6e9e1c66c834e3b2dc2c62c28aff55d
    Author: BuiPhuong2904 <phuongbui290405@gmail.com>
    Date:   Mon Nov 10 21:47:41 2025 +0700
    
        Refactor admin routes and sidebar links
    
        Moved all admin-related routes to a dedicated routes/admin.php file and registered it in the application bootstrap. Updated sidebar links to use resourceful route names. Added IsAdmin middleware and registered it. Removed redundant admin route definitions from web.php for cleaner separation.

--- webgym/bootstrap/app.php
+++ webgym/bootstrap/app.php
@@ -3,16 +3,28 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route; // <--- THÊM DÒNG NÀY
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        
+        // Đăng ký route admin 
+        then: function () {
+            Route::middleware('web')->group(base_path('routes/admin.php'));
+        }
+
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        
+        // Đăng ký middleware IsAdmin
+        $middleware->alias([
+            'admin.role' => \App\Http\Middleware\IsAdmin::class,
+        ]);
+
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
--- webgym/routes/web.php
+++ webgym/routes/web.php
@@ -4,13 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ChatbotController;
 
+use App\Http\Controllers\AuthController;
+
+// Web Routes
+
+// Trang chủ
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/chatbot/message', [ChatbotController::class, 'chat'])->name('chatbot.message');
 
-Route::get('/about', function () {  return view('about');})->name('about');
-Route::get('/contact', function () {  return view('contact');})->name('contact');
+// Giới thiệu & Liên hệ
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
 
-//blog
+// Blog (tĩnh view)
 Route::get('/blog1', function () { return view('blogs.blog_1'); })->name('blog1');
 Route::get('/blog2', function () { return view('blogs.blog_2'); })->name('blog2');
 Route::get('/blog3', function () { return view('blogs.blog_3'); })->name('blog3');
