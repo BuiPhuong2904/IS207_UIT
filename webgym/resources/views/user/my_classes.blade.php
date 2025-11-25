02bcc5fcbbf0864124393b24dc8387efb9d44e1c
Thanh Mai <151462737+Mywayyy@users.noreply.github.com>
2025-11-25 18:25:16 +0700

    user dashboard

---
+++ webgym/resources/views/user/my_classes.blade.php
@@ -0,0 +1,16 @@
+@extends('user.layouts.user_dashboard')
+
+@section('title', 'Lớp học đã đăng ký - GRYND')
+
+@section('content')
+
+<div class="mb-8">
+    <h1 class="text-2xl font-bold text-black font-montserrat">Lớp học của bạn</h1>
+    <p class="text-sm text-gray-500 mt-1 font-open-sans">Quản lý lớp học đã đăng ký</p>
+</div>
+
+<div class="bg-white rounded-2xl shadow-sm p-6 border border-gray-100 flex flex-col md:flex-row items-center gap-6 font-open-sans">
+    <h1>Nội dung lớp hc</h1>
+</div>
+
+@endsection
---
+++ webgym/resources/views/user/profile.blade.php
@@ -0,0 +1,17 @@
+@extends('user.layouts.user_dashboard')
+
+@section('title', 'Hồ sơ người dùng - GRYND')
+
+@section('content')
+           
+<div class="mb-8">
+    <h1 class="text-2xl font-bold text-black font-montserrat">Hồ sơ của bạn</h1>
+    <p class="text-sm text-gray-500 mt-1 font-open-sans">Quản lý thông tin cá nhân và cài đặt mật khẩu</p>
+</div>
+
+<div class="bg-white rounded-2xl shadow-sm p-6 border border-gray-100 flex flex-col md:flex-row items-center gap-6 font-open-sans">
+    <h1>Nội dung</h1>
+</div>
+
+
+@endsection
