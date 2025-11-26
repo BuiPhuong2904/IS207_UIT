f75c7fe9c9197f2b6f7cdc5e1234e5810079456f
Thanh Mai <151462737+Mywayyy@users.noreply.github.com>
2025-11-26 14:10:58 +0700

    Update my_packages.blade.php

--- webgym/resources/views/user/my_packages.blade.php
+++ webgym/resources/views/user/my_packages.blade.php
@@ -2,7 +2,8 @@
 
 @section('title', 'Gói tập đã mua - GRYND')
 
-<script src="//unpkg.com/alpinejs" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+<!-- <script src="//unpkg.com/alpinejs" defer></script> -->
 
 @section('content')
 
