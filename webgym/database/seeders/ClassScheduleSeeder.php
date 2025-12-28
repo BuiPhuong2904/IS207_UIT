be643579cd86a25a0560fcbdbbc0756620095e60
BuiPhuong2904 <phuongbui290405@gmail.com>
2025-12-28 23:20:17 +0700

    Update ClassScheduleSeeder.php

--- webgym/database/seeders/ClassScheduleSeeder.php
+++ webgym/database/seeders/ClassScheduleSeeder.php
@@ -20,7 +20,7 @@ class ClassScheduleSeeder extends Seeder
         DB::table('class_registration')->truncate();
         DB::table('class_schedule')->truncate();
 
-        $today = Carbon::parse('2025-12-15');
+        $today = Carbon::parse('2025-12-22');
 
         // Tạo lịch học
         DB::table('class_schedule')->insert([
